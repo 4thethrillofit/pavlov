6503842d2343d8fa71eba36830d4d61abf0b1846
sidney zhang <sidazhang89@gmail.com>
2013-07-07 12:32:43 -0700

    query error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include SessionsHelper
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.where(:id => session[:user_id]).first if session[:user_id]
+  end
+  def login_user(user)
+    session[:user_id] = user.id
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,3 @@
 module SessionsHelper
-  def current_user
-    @current_user ||= User.where(session[:user_id]).first if session[:user_id]
-  end
-  def login_user(user)
-    session[:user_id] = user.id
-  end
+
 end
